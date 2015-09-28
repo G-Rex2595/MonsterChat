237852f5ec08592d6bfd4aa763f32c3f806b6ab0
MetaY <myoungeta@gmail.com>
2015-09-28 14:46:17 -0400

    Added P2PManager class

---
+++ WiFinder Project/Networking/P2PManager.java
@@ -0,0 +1,17 @@
+public class P2PManager {
+	
+	/* Singleton object, no instantiation */
+	private P2PManager() { }
+	
+	
+	/**
+	  *
+	  */
+	public void sendMessage(Message msg) {
+			
+		
+	}
+	
+	
+	
+}
