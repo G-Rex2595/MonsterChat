d252f63c20935a1c962161e89cede45933bd1370
MetaY <myoungeta@gmail.com>
2015-09-28 14:54:16 -0400

    Updated P2PManager

--- WiFinder Project/Networking/P2PManager.java
+++ WiFinder Project/Networking/P2PManager.java
@@ -1,17 +1,37 @@
+import java.util.LinkedList;
+
+/**
+  * @author Michael Young
+  */
 public class P2PManager {
+	//Fields
+	private String roomName;
+	private String passwd;
+	private LinkedList<Message> incomingMessageQueue;
+	private LinkedList<Message> outgoingMessageQueue;
 	
-	/* Singleton object, no instantiation */
+	//Singleton object, no instantiation
 	private P2PManager() { }
-	
 	
 	/**
 	  *
 	  */
 	public void sendMessage(Message msg) {
-			
 		
 	}
 	
+	/**
+	  *
+	  */
+	public void setPassword(String passwd) {
+		this.passwd = passwd;
+	}
 	
+	/**
+	  * 
+	  */
+	public void setRoom(String roomName) {
+		this.roomName = roomName;
+	}
 	
 }
